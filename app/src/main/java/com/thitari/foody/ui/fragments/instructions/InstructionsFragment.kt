497191e84c72a78eb41e82804e4587252959baf3
thitari <thitari.sam@gmail.com>
2021-11-17 08:49:03 +0700

    Implement InstructionsFragment

--- app/src/main/java/com/thitari/foody/ui/fragments/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/thitari/foody/ui/fragments/instructions/InstructionsFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import com.thitari.foody.R
+import com.thitari.foody.models.Result
+import com.thitari.foody.util.Constants
+import kotlinx.android.synthetic.main.fragment_instructions.view.*
 
 class InstructionsFragment : Fragment() {
 
@@ -15,5 +19,15 @@ class InstructionsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_instructions, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val args = arguments
+        val bundle: Result? = args?.getParcelable(Constants.RECIPE_RESULT_KEY)
+
+        view.webViewInstructions.webViewClient = object : WebViewClient() {}
+        val websiteUrl: String = bundle!!.sourceUrl
+        view.webViewInstructions.loadUrl(websiteUrl)
+        super.onViewCreated(view, savedInstanceState)
     }
 }
